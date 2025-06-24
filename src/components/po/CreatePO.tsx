da8b28317f0f8dc45d03ba165eaac801ce990e62
Samarth Mahapatra <smahapatra@berkeley.edu>
2025-06-24 00:49:34 -0400

    Updated CreatePO.tsx

--- src/components/po/CreatePO.tsx
+++ src/components/po/CreatePO.tsx
@@ -621,7 +621,7 @@ export const CreatePO: React.FC = () => {
                       />
                     </div>
                   </div>
-                  <div className="lg:col-span-3">
+                  <div className="lg:col-span-2">
                     <label className="block text-xs font-medium text-gray-300 mb-1">Link (Optional)</label>
                     <div className="flex">
                       <input
